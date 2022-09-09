74daecbc680dae794fc68df820d5c3f4f2e6b824
NicolasLacerda <nicolas.lacerda.work@gmail.com>
2022-09-09 14:41:49 -0300

    Create listaDeAlunos.js

---
+++ listaDeAlunos.js
@@ -0,0 +1,17 @@
+let numeroDeAlunos = 10
+
+for (let contador = 0; contador < numeroDeAlunos; contador++) {
+    
+    if (contador == 0) {
+        console.log("Zero")
+    }
+
+    else if (contador %2 == 0) {
+        console.log("Número par")
+    }
+
+    else {
+        console.log("Número impar")
+    }
+
+}
